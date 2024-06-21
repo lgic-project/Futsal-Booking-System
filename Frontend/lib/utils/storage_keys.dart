7b1c54f2571149827c000c7fd7b1b0b83e17b3d2
Manish-Shrestha14 <manishstha273@gmail.com>
2024-06-21 20:48:51 +0545

    update

--- Frontend/lib/utils/storage_keys.dart
+++
@@ -1,7 +0,0 @@
-class StorageKeys {
-  // ignore: constant_identifier_names
-  static const String USER = "user";
-  static const String ACCESS_TOKEN = "accessToken";
-
-  static const String APP_LOAD_DATE = "app_load_date";
-}
